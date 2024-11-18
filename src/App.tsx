25f58e50df342d408e88546ea1dd53e1d6351267
Sebastián Fretes <sebafretesca@gmail.com>
2024-11-17 21:37:15 -0300

    first css styles added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ function App() {
 
   return (
     <>
-      <h1>CriptoApp</h1>
+      <div className="container">
+        <h1 className="app-title">Cotizador de <span>Criptomonedas</span></h1>
+
+        <div className="content">
+
+        </div>
+      </div>
     </>
   )
 }
